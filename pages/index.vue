513d7fb8d5cc77c428c9de65a0280e674007b952
ttaniguchi <ttaniguchi@oikaze.jp>
2019-07-18 23:15:55 +0900

    Update search機能の追加

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,24 @@
   <div>
     <section class="l-section -is-medium">
       <div class="container">
+        <h2 class="c-articleTitle">Search</h2>
+        <form @submit="checkForm" method="post">
+          <input v-model="text" />
+          <input type="submit" value="value" />
+        </form>
+         <ul class="c-card">
+          <li v-for="result in resultSearch" :key="result.id" class="c-card_item">
+            <div class="c-card_inner">
+              <img :src="`https://image.tmdb.org/t/p/w780/${result.backdrop_path}`" >
+              <div class="c-card_desc">
+                  <div class="c-card_desc_inner">
+                    <h3>{{result.title}}</h3>
+                    <p>{{ result.overview.slice(0,100)}}...</p>
+                  </div>
+              </div>
+            </div>
+          </li>
+        </ul>
         <h2 class="c-articleTitle">Most popular movies</h2>
         <ul class="c-card">
           <li v-for="result in results" :key="result.id" class="c-card_item">
@@ -29,7 +47,20 @@
 			return {
         results
 			}
-		}
+    },
+    data(){
+      return{
+        resultSearch: '',
+        text: ''
+      }
+    },
+    methods: {
+      async checkForm(e) {
+        e.preventDefault();
+        const response = await this.$axios.$get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_KEY}&query=コナン&language=ja-JA`);
+        this.resultSearch = response.results;
+      }
+  },
 	}
 </script>
 
