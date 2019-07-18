374cd7a213fb31f45ca0768880590712d3367e5a
ttaniguchi <ttaniguchi@oikaze.jp>
2019-07-18 14:51:28 +0900

    Update footerテンプレートの追加

---
+++ components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="l-footer">
+    <div class="l-footer_inner">
+      <ul class="c-listSns">
+        <li class="c-listSns_item">
+          <a href="https://twitter.com/RinstarskyKujat">
+           <img src="/inc-tw.svg" alt="Twitter">
+          </a>
+        </li>
+        <li class="c-listSns_item">
+          <a href="https://github.com/TakuyaTaniguchi">
+           <img src="/icon-github.svg" alt="Github">
+          </a>
+        </li>
+      </ul>
+    </div>
+</section>
+</template>
+
+<script>
+export default {
+  name: 'footer',
+}
+</script>
