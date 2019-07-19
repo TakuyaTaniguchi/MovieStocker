4bafbcbe0bc1e08d57afcd8f6d62e4ad19b680f6
ttaniguchi <ttaniguchi@oikaze.jp>
2019-07-19 15:36:32 +0900

    Update 値が無い時の判定を追加

--- pages/index.vue
+++ pages/index.vue
@@ -10,12 +10,14 @@
          <ul class="c-card">
           <li v-for="result in resultSearch" :key="result.id" class="c-card_item">
             <nuxt-link :to="`/page/${result.id}`">
-              <div class="c-card_inner">
-                <img :src="`https://image.tmdb.org/t/p/w780/${result.backdrop_path}`" >
+               <div class="c-card_inner">
+                <img v-if="!result.backdrop_path" src="/dummy.jpg">
+                <img  v-else :src="`https://image.tmdb.org/t/p/w780/${result.backdrop_path}`" >
                 <div class="c-card_desc">
                     <div class="c-card_desc_inner">
                       <h3 class="c-card_desc_title">{{result.title}}</h3>
-                      <p>{{ result.overview.slice(0,100)}}...</p>
+                      <p v-if="!result.overview" class="c-card_desc_text">Sorry No OverView</p>
+                      <p v-else class="c-card_desc_text">{{ result.overview.slice(0,100)}}...</p>
                     </div>
                 </div>
               </div>
@@ -29,11 +31,13 @@
           <li v-for="result in results" :key="result.id" class="c-card_item">
             <nuxt-link :to="`/page/${result.id}`">
               <div class="c-card_inner">
-                <img :src="`https://image.tmdb.org/t/p/w780/${result.backdrop_path}`" >
+                <img v-if="!result.backdrop_path" src="/dummy.jpg">
+                <img  v-else :src="`https://image.tmdb.org/t/p/w780/${result.backdrop_path}`" >
                 <div class="c-card_desc">
                     <div class="c-card_desc_inner">
                       <h3 class="c-card_desc_title">{{result.title}}</h3>
-                      <p class="c-card_desc_text">{{ result.overview.slice(0,100)}}...</p>
+                      <p v-if="!result.overview" class="c-card_desc_text">Sorry No OverView</p>
+                      <p v-else class="c-card_desc_text">{{ result.overview.slice(0,100)}}...</p>
                     </div>
                 </div>
               </div>
@@ -50,6 +54,7 @@
     async asyncData({app}){
       const data = await app.$axios.$get(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${process.env.TMDB_KEY}&language=ja-JA`);
       const results = data.results;
+       console.log(results);
 			return {
         results
 			}
@@ -65,7 +70,6 @@
         e.preventDefault();
         const text = this.text;
         const response = await this.$axios.$get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_KEY}&query=${text}&language=ja-JA`);
-        console.log(response);
         this.resultSearch = response.results;
       }
   },
