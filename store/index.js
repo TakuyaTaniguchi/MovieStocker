48ec6baec8d160fb4aa85f3f52be76cb24d22377
ttaniguchi <ttaniguchi@oikaze.jp>
2019-07-22 09:48:49 +0900

    Add storeの打ち込み

--- store/index.js
+++ store/index.js
@@ -1,3 +1,18 @@
-export const state = () => ({
-    isLoading: false
-})
+export const state = () => {
+    counter: 0
+}
+
+export const mutaitions = {
+    increment (state) {
+        state.counter++
+    }
+}
+// export const state = () => ({
+//     isLoading: false
+// })
+
+// export const mutaitions = {
+//     setIsLoading(state,isLoading){
+//         state.isLoading = isLoading
+//     }
+// }
---
+++ store/users.js
@@ -0,0 +1,15 @@
+// export const state = () => ({
+//     list: []
+// })
+
+// export const mutaitions = {
+//     adduser(state,user){
+//         state.list.push(user)
+//     }
+// }
+
+// export const actions = {
+//     adduser({ commit },{ user }){
+//         commit('addUser',user)
+//     }
+// }
